dfb9c02864c92df658adfb94a6a34d5330c47ae2
sonysj <softyr200500@gmail.com>
2024-10-02 14:41:45 +0500

    dfhbfhyn 19

---
+++ task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace task_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первый член геометрической прогрессии:");
+        double b1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите знаменатель геометрической прогрессии:");
+        double q = double.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+        Console.WriteLine("Введите номер для вычисления соответствующего члена прогрессии:");
+        double n = double.Parse(Console.ReadLine());
+
+        double bn = b1 * Math.Pow(q, n - 1);
+        Console.WriteLine(bn);
+
+        Console.ReadKey();
+
+    }
+}
